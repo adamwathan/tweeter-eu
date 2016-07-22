09d1e8f7c9b24cc7eb3a221577c05953cc5b696a
Adam Wathan <adam.wathan@gmail.com>
2016-07-22 15:32:31 -0400

    Init

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a given Closure or controller and enjoy the fresh air.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});

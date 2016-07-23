4605a1bc96de8f5004ed704a4f512657c58b0a66
Adam Wathan <adam.wathan@gmail.com>
2016-07-23 19:45:20 -0400

    Improve some styling, fix login redirect

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,12 +4,14 @@
 <div class="container">
     <div class="row">
         <div class="col col-sm-8 col-sm-offset-2">
-            <div class="jumbotron text-center">
-                <h1>
-                    Tweeter
-                </h1>
-                <p>Our tweets go to 141.</p>
-                <a href="/sign-up" class="btn btn-success btn-lg">Create an account</a>
+            <div class="panel panel-default u-pa-3">
+                <div class="panel-body text-center">
+                    <h1>
+                        Tweeter
+                    </h1>
+                    <p>Our tweets go to 141.</p>
+                    <a href="/sign-up" class="btn btn-primary btn-lg">Create an account</a>
+                </div>
             </div>
         </div>
     </div>

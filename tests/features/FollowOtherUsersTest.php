a1165e0b019ac68b1a8d1b29beeef18c49c8b5eb
Adam Wathan <adam.wathan@gmail.com>
2016-07-24 12:45:36 -0400

    wip

--- tests/features/FollowOtherUsersTest.php
+++ tests/features/FollowOtherUsersTest.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\User;
+use Illuminate\Mail\Message;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
@@ -19,5 +21,16 @@ class FollowOtherUsersTest extends TestCase
 
         $this->assertRedirectedToRoute('following.index');
         $this->assertTrue($user->follows($userToFollow));
+    }
+
+    public function test_a_user_receives_a_notification_email_when_they_are_followed()
+    {
+        $user = factory(User::class)->create(['username' => 'follower']);
+        $userToFollow = factory(User::class)->create(['username' => 'to-follow']);
+
+        $this->actingAs($user)
+            ->post('/following', ['username' => 'to-follow']);
+
+
     }
 }

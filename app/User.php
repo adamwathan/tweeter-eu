45330e8a7a47b14fa6b6c4e9e63f3b186e3f5fb9
Adam Wathan <adam.wathan@gmail.com>
2016-08-21 22:02:49 +0200

    Add test for user profile composer

--- app/User.php
+++ app/User.php
@@ -14,7 +14,7 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $guarded= [];
+    protected $guarded = [];
 
     /**
      * The attributes that should be hidden for arrays.

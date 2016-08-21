0e83ac5def43b6948c64b2472757f39a100e655b
Adam Wathan <adam.wathan@gmail.com>
2016-08-21 21:18:07 +0200

    Add blank slate text to users.show page

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -6,11 +6,15 @@
         <h4 class="u-mt-0 u-mb-0">Tweets by {{ "@{$user->username}" }}</h4>
     </li>
 
-    @foreach ($tweets as $tweet)
+    @forelse ($tweets as $tweet)
     <li class="list-group-item list-group-item-lg">
         @include('tweets.partials.show', ['tweet' => $tweet])
     </li>
-    @endforeach
+    @empty
+    <li class="list-group-item list-group-item-lg text-center">
+        It looks like {{ "@{$user->username}" }} hasn't tweeted yet!
+    </li>
+    @endforelse
 </ul>
 @endsection
 

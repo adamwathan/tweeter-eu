55992b663efda405512e7cc539cafbfd8ca7f996
Adam Wathan <adam.wathan@gmail.com>
2016-08-21 19:44:23 +0200

    Use View facade

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,12 +15,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        view()->composer('partials.who-to-follow', function ($view) {
+        View::composer('partials.who-to-follow', function ($view) {
             $users = Auth::user()->notFollowing()->latest()->limit(5)->get();
             $view->with('users', $users);
         });
 
-        view()->composer('partials.user-profile', function ($view) {
+        View::composer('partials.user-profile', function ($view) {
             $view->with('tweetCount', $view->user->tweets()->count());
             $view->with('followerCount', $view->user->followers()->count());
             $view->with('followingCount', $view->user->following()->count());

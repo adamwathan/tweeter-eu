8c4c42075096ae48e6ec46108c979768ff7d36e7
Adam Wathan <adam.wathan@gmail.com>
2016-08-21 23:56:54 +0200

    Add test for non-user specific timeline

---
+++ tests/features/ViewTimelineTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\User;
+use App\Tweet;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ViewTimelineTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function test_viewing_main_timeline()
+    {
+        $john = factory(User::class)->create(['username' => 'john']);
+        $john->tweets()->save(factory(Tweet::class)->make(['body' => "john's tweet"]));
+        factory(User::class)->create(['username' => 'jane'])
+            ->tweets()->save(factory(Tweet::class)->make(['body' => "jane's tweet"]));
+        factory(User::class)->create(['username' => 'steve'])
+            ->tweets()->save(factory(Tweet::class)->make(['body' => "steve's tweet"]));
+
+        $this->actingAs($john)
+            ->visit('/')
+            ->see("john's tweet")
+            ->see("jane's tweet")
+            ->see("steve's tweet");
+    }
+}
